5654cac852e86b353778b80ef789a4e516aeb9ff
RKH <rhysh6@gmail.com>
2020-09-13 06:45:20 +0100

    Updates

---
+++ precache-manifest.0db1d6ea568ea00b1e71c88a76693ec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b347098def92545c368795a69025cee",
+    "url": "/howardhavens/index.html"
+  },
+  {
+    "revision": "a0fdd9fc89ec5132ddb3",
+    "url": "/howardhavens/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a74f24550879072f385",
+    "url": "/howardhavens/static/js/2.386a6dd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/howardhavens/static/js/2.386a6dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fdd9fc89ec5132ddb3",
+    "url": "/howardhavens/static/js/main.68ffb1e0.chunk.js"
+  },
+  {
+    "revision": "cbfde94ac3bd448a1df5",
+    "url": "/howardhavens/static/js/runtime-main.2784b61b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/howardhavens/static/media/logo.5d5d9eef.svg"
+  }
+]);
