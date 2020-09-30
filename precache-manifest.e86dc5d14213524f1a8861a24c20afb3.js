f34c8e4b1401071eb6e2aa05edf39b82d74fc6a5
RKH <rhysh6@gmail.com>
2020-09-30 20:26:32 +0100

    Updates

---
+++ precache-manifest.e86dc5d14213524f1a8861a24c20afb3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54ea6e28176f644877278c2c78d3317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1db2f3bde71f07481e5",
+    "url": "/static/css/main.931c8de3.chunk.css"
+  },
+  {
+    "revision": "2fa6fdc1f7b97a91a00d",
+    "url": "/static/js/2.aa8b21c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa8b21c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1db2f3bde71f07481e5",
+    "url": "/static/js/main.d42cc8e8.chunk.js"
+  },
+  {
+    "revision": "4e4ed6a48a9289436cec",
+    "url": "/static/js/runtime-main.e05e83d2.js"
+  },
+  {
+    "revision": "dd26c647553b1ab8110da9d928ea0a7f",
+    "url": "/static/media/Check_Icon.dd26c647.svg"
+  },
+  {
+    "revision": "8c8521de0a6ef7b932fad842b51b3450",
+    "url": "/static/media/RKHT_logo.8c8521de.png"
+  },
+  {
+    "revision": "ef8736a83fa1443f25542f991d0d1539",
+    "url": "/static/media/deal.ef8736a8.jpg"
+  },
+  {
+    "revision": "e01940f8a206ac9034971b0c130429a2",
+    "url": "/static/media/explaining.e01940f8.jpg"
+  },
+  {
+    "revision": "fbaeb946c4f260016cd394d253c92c44",
+    "url": "/static/media/growPounds.fbaeb946.jpg"
+  },
+  {
+    "revision": "42a310fa0b5a5ebd0347dd29ec7b3dda",
+    "url": "/static/media/house1.42a310fa.jpg"
+  },
+  {
+    "revision": "b078b5b93d7c60fdf6265f6102adea5d",
+    "url": "/static/media/house2.b078b5b9.jpg"
+  },
+  {
+    "revision": "0cd80aa61a3a26bbd081859cac017921",
+    "url": "/static/media/logo.0cd80aa6.svg"
+  },
+  {
+    "revision": "7de22b224084795fa2f4e34b2ec5a3a9",
+    "url": "/static/media/logoWhite.7de22b22.svg"
+  },
+  {
+    "revision": "c30073cecb3b38bac971004222d248bb",
+    "url": "/static/media/talkThrough.c30073ce.jpg"
+  },
+  {
+    "revision": "13cd47a5e14b936b36fd0aaea446ab03",
+    "url": "/static/media/tenantBuyers.13cd47a5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/howardhavens/precache-manifest.9d0a89d5ee227d71d9857e786409986e.js"
+  "/precache-manifest.e86dc5d14213524f1a8861a24c20afb3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/howardhavens/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
